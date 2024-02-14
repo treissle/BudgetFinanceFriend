b0fe11233b74e7eb5dfdabb5e2aee37f93a44dda
treissle <157082749+treissle@users.noreply.github.com>
2024-02-14 12:38:47 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+
+app = Flask(__name__)
+
+app.secret_key = 'cactus'
+
+app.config['MYSQL_HOST'] = '107.180.1.16'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'spring2024Cteam10'
+app.config['MYSQL_PASSWORD'] = 'spring2024Cteam10'
+app.config['MYSQL_DB'] = 'spring2024Cteam10'
+
+mysql = MySQL(app)
+
+@app.route('/')
+@app.route('/login', methods =['GET','POST'])
+def login():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+		username = request.form['username']
+		password = request.form['password']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM 401K WHERE username = % s AND password = % s', (username, password, ))
+		account = cursor.fetchone()
+		if account:
+			session['loggedin'] = True
+			session['id'] = account['id']
+			session['username'] = account['username']
+			msg = 'Logged in successfully !'
+			return render_template('401k.html', msg = msg)
+		else:
+			msg = 'Incorrect username and/or password, please try again'
+	return render_template('login.html', msg = msg)
+
+@app.route('/logout')
+def logout():
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
+
+@app.route('/register', methods =['GET','POST'])
+def register():
+	msg = ''
+	if request.method == 'POST' and 'username' in request.form and 'password' in request.form :
+		username = request.form['username']
+		password = request.form['password']
+		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+		cursor.execute('SELECT * FROM 401K WHERE username = % s', (username, ))
+		account = cursor.fetchone()
+		if account:
+			msg = 'Account already exists !'
+		else:
+			cursor.execute('INSERT INTO 401K VALUES (NULL, % s, % s)', (username, password, ))
+			mysql.connection.commit()
+			msg = 'You have successfully registered'
+			return render_template('login.html', msg = msg)
+	return render_template('register.html', msg = msg)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
