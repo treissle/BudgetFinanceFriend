726a673724326699aaccb2529ba81729c02c1425
lizziemckinnon <120137573+lizziemckinnon@users.noreply.github.com>
2024-02-23 18:20:08 +0000

    updated files

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 
-
 app = Flask(__name__)
-
 app.secret_key = 'cactus'
 
 app.config['MYSQL_HOST'] = '107.180.1.16'
@@ -12,28 +10,53 @@ app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_USER'] = 'spring2024Cteam10'
 app.config['MYSQL_PASSWORD'] = 'spring2024Cteam10'
 app.config['MYSQL_DB'] = 'spring2024Cteam10'
-
 mysql = MySQL(app)
 
 @app.route('/')
-@app.route('/login', methods =['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	msg = ''
-	if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-		username = request.form['username']
-		password = request.form['password']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM 401K WHERE username = % s AND password = % s', (username, password, ))
-		account = cursor.fetchone()
-		if account:
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
-			return render_template('401k.html', msg = msg, username=username)
-		else:
-			msg = 'Incorrect username and/or password, please try again'
-	return render_template('login.html', msg = msg)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM 401K WHERE username = %s AND password = %s', (username, password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+      
+            session['user_data'] = {
+                'currentAge': account['currentAge'],
+                'currentSalary': account['currentSalary'],
+                'currentBalance': account['currentBalance'],
+                'contributionPercentage': account['contributionPercentage'],
+                'employerMatch': account['employerMatch'],
+                'employerMatchLimit': account['employerMatchLimit'],
+                'retirementAge': account['retirementAge']
+            }
+            return redirect(url_for('home'))  
+        else:
+            msg = 'Incorrect username/password!'
+            return render_template('login.html', msg=msg)
+    return render_template('login.html', msg='')
+
+@app.route('/home')
+def home():
+    username = session['username']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    isSaved = cursor.execute("SELECT saved FROM 401K WHERE username = %(username)s",{'username': username})
+    result = cursor.fetchone()
+    saved = result['saved']
+    
+    if 'loggedin' in session:
+        if ('user_data' in session and session['user_data']) and saved:
+            restore_session = True
+        else:
+            restore_session = False
+
+        return render_template('401k.html', user_data=session.get('user_data', {}), restore_session=restore_session)
+    return redirect(url_for('login'))
 
 @app.route('/save_user_inputs', methods=['POST'])
 def save_user_inputs():
@@ -59,7 +82,8 @@ def save_user_inputs():
                 contributionPercentage = %(contributionPercentage)s,
                 employerMatch = %(employerMatch)s,
                 employerMatchLimit = %(employerMatchLimit)s,
-                retirementAge = %(retirementAge)s
+                retirementAge = %(retirementAge)s,
+                saved = 1
             WHERE username = %(username)s
         """, {'username': username, **user_inputs})
 
@@ -89,7 +113,7 @@ def register():
 		if account:
 			msg = 'Account already exists !'
 		else:
-			cursor.execute('INSERT INTO 401K VALUES (NULL, % s, % s, NULL, NULL, NULL, NULL, NULL, NULL, NULL)', (username, password, ))
+			cursor.execute('INSERT INTO 401K VALUES (NULL, % s, % s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0)', (username, password, ))
 			mysql.connection.commit()
 			msg = 'You have successfully registered'
 			return render_template('login.html', msg = msg, username=username)
